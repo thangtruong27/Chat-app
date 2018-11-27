8116756178d8d739e1b69cee4602ccf3d5b4c57b
Thang Truong <truongnt0279@gmail.com>
2018-11-27 11:52:54 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bodyReq = require('body-parser');
+const port = 3000;
+
+var users = [];
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+
+app.use(bodyReq.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.post('/message', function (req, res) {
+    users.push(req.body.nickname);
+    res.render('message', { nickname: req.body.nickname});
+});
+
+io.on('connection', function (socket) {
+    console.log('A user connected');
+    console.log(users);
+    socket.on('new message', function (msg) {
+        socket.emit('new message', msg);
+        socket.broadcast.emit('new message', msg);
+        console.log(msg);
+    });
+    
+    socket.on('typing', function (nickname) {
+        socket.broadcast.emit('typing', nickname);
+    });
+
+    socket.on('stop typing', function( nickname){
+        socket.broadcast.emit('stop typing', nickname);
+    });
+
+    socket.on('disconnect', () => console.log('User disconnected'));
+});
+
+
+http.listen(port, () => console.log("Server is running on port 3000"));
+
