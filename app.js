846a45379cba435b9c79af93a938f7ae85cdd901
Thang Truong <truongnt0279@gmail.com>
2018-12-13 09:36:03 +0700

    add actives users and change view

--- app.js
+++ app.js
@@ -4,34 +4,50 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const bodyReq = require('body-parser');
+const morgan =  require('morgan');
 const port = 3000;
 
-var msgs = [];
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 
 app.use(bodyReq.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
 /*  Route   */
 app.get('/', function (req, res) {
     res.render('index');
 });
-
-app.post('/message', function (req, res) {
-    res.render('message', { nickname: req.body.nickname });
+app.post('/messenger', function (req, res) {
+    res.render('messenger', { nickname: req.body.nickname });
 });
 
+app.get('/messenger', (req,res)=>{
+    res.render('messenger');
+});
+
+var msgs = [];
+var users = [];
 /*  Connection Handle  */
 io.on('connection', function (socket) {
-    console.log('A user connected');    
+    console.log('A user connected');        
     io.to(socket.id).emit('add participants messages', msgs);
+    io.to(socket.id).emit('add active users', users);    
+
+    socket.on('new active user', function(nickname){
+        users.push({
+            nickname: nickname,
+            socketId: socket.id
+        });
+        io.emit('new active user', nickname);
+        console.log(users);             
+    });
 
     socket.on('new message', function (msg) {
-        socket.emit('new message', msg);
         socket.broadcast.emit('new message', msg);
         msgs.push(msg);
+        console.log(msg);        
     });
 
     socket.on('typing', function (nickname) {
@@ -42,7 +58,7 @@ io.on('connection', function (socket) {
     });
 
     socket.on('disconnect', () => console.log('User disconnected'));
-});
+});         
 
 
 http.listen(port, () => console.log("Server is running on port 3000"));
